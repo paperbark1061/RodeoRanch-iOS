5695aee274a5861d801497f42807a4effc3a9bdc
paperbark1061 <paperbark1061@duck.com>
2026-03-30 19:16:12 +1100

    Force light mode: preferredColorScheme(.light) in app + UIUserInterfaceStyle=Light in plist

--- RodeoRanch/App/RodeoRanchApp.swift
+++ RodeoRanch/App/RodeoRanchApp.swift
@@ -10,7 +10,7 @@ struct RodeoRanchApp: App {
             ContentView()
                 .environmentObject(authState)
                 .environmentObject(appRouter)
-                .preferredColorScheme(.dark)
+                .preferredColorScheme(.light)
         }
     }
 }
